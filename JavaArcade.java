16054c896663d89b24901ea5bb4d5a69810bd331
AD <75181003+AD8787@users.noreply.github.com>
2026-04-25 18:00:50 -0700

    Remove read-me doc from JavaArcade.java
    Removed the read-me documentation for the Hangman game.

--- JavaArcade.java
+++ JavaArcade.java
@@ -1,16 +1,3 @@
-/*
-*** READ-ME DOC FOR <Hangman> ***
-Game Title: Hangman
-Created by: Aarush Dhawan, Anurag Chatterjee
-Period #3 CSA
-
-Instructions:
-- The computer selects a word from a word bank, and then a player tries to guess letters in that word. Each wrong guess makes the figure “hang more (i guess)” and basically the user has a certain number of guesses to figure out the word.
-- The player guesses letters, and the game reveals correct letters in their positions and tracks incorrect guesses. The player continues guessing until the word is fully revealed or the allowed number of wrong guesses is reached.
-- The player wins if they guess the word before reaching the max number of incorrect guesses. The game ends if the player reaches the max number of wrong guesses without completing the word.
-- The game can be played multiple times, and the player can choose to play again after each round.
-*/
-
 import java.util.*; 
 
 public class JavaArcade {
